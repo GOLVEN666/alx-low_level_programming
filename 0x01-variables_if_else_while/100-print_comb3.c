dbecfb2281e2bd22b874e5e128180a8db2a543b5
axe <78966503+GOLVEN666@users.noreply.github.com>
2023-03-18 03:06:04 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,23 +9,24 @@
 /**main-Entrypoint.Printsallpossibledifferentcombinationsoftwodigits.
 *Return:0onsuccess.
 */
-intmain(void)
+int main(void)
 {
-    intnum1,num2;
+	int num1, num2;
 
-    for(num1=0;num1<=9;num1++)
-    {
-    for(num2=num1+1;num2<=9;num2++)
-    {
-    putchar(num1+'0');
-    putchar(num2+'0');
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 57; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
 
-    if(!(num1==8&&num2==9))
-    {
-        putchar(',');
-        putchar(' ');}
-    }
-}
-putchar('\n');
-return(0);
+			if (!(num1 == 8 && num2 == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
