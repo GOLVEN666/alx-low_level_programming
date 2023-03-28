8f4e4f976e9cbf005cb1934118c78fe3a3169faa
GOLVEN666 <achraf.xchahid@gmail.com>
2023-03-27 18:27:50 -0700

    ux

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,18 +6,21 @@
  * Return: 0 Always.
  * 
  */
-
 int main(void)
 {
-	srand(time(NULL)); // Seed random number generator
-
 	int sum = 0;
+	int r;
+	time_t t;
+
+	srand((unsigned int) time(&t)); // Seed random number generator
+
 	while (sum < 2772) {
-		int r = rand() % 128; // Generate a random number between 0 and 127
+		r = rand() % 128; // Generate a random number between 0 and 127
 		putchar(r);
-		 sum += r;
+		sum += r;
 	}
 	putchar(2772 - sum); // Add remaining character to sum
 
 	return 0;
 }
+
